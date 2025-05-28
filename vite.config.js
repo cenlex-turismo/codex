621d60fe96df7ead7b80f3ad80358be07b9ee945
root <celex.turismo@gmail.com>
2025-05-28 21:57:02 +0000

    Generate sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import Sitemap from 'vite-plugin-sitemap'
+
 
 export default defineConfig({
-    plugins: [react(), tailwindcss(), flowbiteReact()],
+    plugins: [react(), tailwindcss(), flowbiteReact(),
+    Sitemap({
+        hostname: 'https://codex.celexest.com', // Your site's URL
+        outDir: './dist', // The output directory for the sitemap
+    }),],
     css: {
         postcss: './postcss.config.js',
     },
